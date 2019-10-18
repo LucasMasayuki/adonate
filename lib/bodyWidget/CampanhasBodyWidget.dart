6455596ad2c26d8a15eb17e880b0292f1cc005d4
Renan Mastropaolo <re.gon.mas@gmail.com>
2019-10-18 18:26:03 -0300

    Começo da campanha

---
+++ lib/bodyWidget/CampanhasBodyWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CampanhasBodyWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CampanhasBodyWidgetState();
+}
+
+class _CampanhasBodyWidgetState extends State<CampanhasBodyWidget> {
+  @override
+  Widget build(BuildContext context) {}
+}
